88831409844bf3c3ca4a67541e715bcca97fece5
joaopaulocordeiro <joaopaulocordeiro21@gmail.com>
2023-08-30 16:42:41 -0300

    criaçãoi de acessibility

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -2,7 +2,7 @@ import { dirname, join } from "path";
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-interactions"), getAbsolutePath("@storybook/addon-mdx-gfm")],
+  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-interactions"), getAbsolutePath("@storybook/addon-mdx-gfm"), getAbsolutePath("@storybook/addon-a11y")],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {}

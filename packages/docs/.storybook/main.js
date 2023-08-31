88fe774a2f0e04fce42452e83181a283d8132486
joaopaulocordeiro <joaopaulocordeiro21@gmail.com>
2023-08-31 12:21:12 -0300

    att main.js

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -12,7 +12,7 @@ const config = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = './';
+      config.base = '/05-design-system/'
     }
 
     return config

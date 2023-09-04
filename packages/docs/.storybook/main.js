9445e1c52ee2a3c94064c0b851ff6bc7a1dbc8b3
joaopaulocordeiro <joaopaulocordeiro21@gmail.com>
2023-09-04 17:14:31 -0300

    correção de nome do respositorio

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -16,7 +16,7 @@ const config = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-desing-system/'
+      config.base = '/05-design-system/'
     }
 
     return config

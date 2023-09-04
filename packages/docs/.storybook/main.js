8b1119e1966e5bb61da4b71115511ec4d376c2fc
joaopaulocordeiro <joaopaulocordeiro21@gmail.com>
2023-09-04 16:39:00 -0300

    correção de nome da pasta no main do storybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType == 'PRODUCTION') {
-      config.base = '/designSystem/'
+      config.base = '/05-design-system/'
     }
 
     return config
